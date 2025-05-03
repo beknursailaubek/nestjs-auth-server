eb291fd33839ed516a3e00aca8d10052304ca683
beknursailaubek <b3knursailaub3k@gmail.com>
2025-05-03 15:08:41 +0500

    feat: add login and logout functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,26 @@
 import {
 	ConflictException,
 	Injectable,
-	InternalServerErrorException
+	InternalServerErrorException,
+	NotFoundException,
+	UnauthorizedException
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { AuthMethod, User } from '@prisma/__generated__'
-import { Request } from 'express'
+import { verify } from 'argon2'
+import { Request, Response } from 'express'
 
 import { UserService } from '@/user/user.service'
 
+import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
 
 @Injectable()
 export class AuthService {
-	public constructor(private readonly userService: UserService) {}
+	public constructor(
+		private readonly userService: UserService,
+		private readonly configService: ConfigService
+	) {}
 
 	public async register(req: Request, dto: RegisterDto) {
 		const isExists = await this.userService.findByEmail(dto.email)
@@ -35,9 +43,43 @@ export class AuthService {
 		return this.saveSession(req, newUser)
 	}
 
-	public async login() {}
+	public async login(req: Request, dto: LoginDto) {
+		const user = await this.userService.findByEmail(dto.email)
 
-	public async logout() {}
+		if (!user || !user.password) {
+			throw new NotFoundException(
+				'Пользователь не найден. Пожалуйста, проверьте введенные данные'
+			)
+		}
+
+		const isValidPassword = await verify(user.password, dto.password)
+
+		if (!isValidPassword) {
+			throw new UnauthorizedException(
+				'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
+			)
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	public async logout(req: Request, res: Response): Promise<void> {
+		return new Promise((resolve, reject) => {
+			req.session.destroy(err => {
+				if (err) {
+					return reject(
+						new InternalServerErrorException(
+							'Не удалось завершить сессию. Возможно, возникла проблема с сервером или сессия уже была завершена.'
+						)
+					)
+				}
+				res.clearCookie(
+					this.configService.getOrThrow<string>('SESSION_NAME')
+				)
+				resolve()
+			})
+		})
+	}
 
 	public async saveSession(req: Request, user: User) {
 		return new Promise((resolve, reject) => {
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
+import { UserRole } from '@prisma/__generated__'
 
-@Controller('user')
+import { Authorization } from '@/auth/decorators/auth.decorator'
+import { Authorized } from '@/auth/decorators/authorized.decorator'
+
+import { UserService } from './user.service'
+
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Authorization()
+	@HttpCode(HttpStatus.OK)
+	@Get('profile')
+	public async findProfile(@Authorized('id') userId: string) {
+		return this.userService.findById(userId)
+	}
+
+	@Authorization(UserRole.ADMIN)
+	@HttpCode(HttpStatus.OK)
+	@Get('by-id/:id')
+	public async findById(@Param('id') id: string) {
+		return this.userService.findById(id)
+	}
 }
